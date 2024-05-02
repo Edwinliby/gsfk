dd0f1bff4302be0699562b82bcfd6bc04c35ffc9
Edwinliby <edwinliby30@gmail.com>
2024-05-02 16:37:04 +0530

    addded buildship api

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from './Home/home';
 import Team from './Team/team';
@@ -9,6 +9,10 @@ const App = () => {
     { path: "/", element: <Home /> },
     { path: "/team", element: <Team /> },
   ]);
+
+  useEffect(() => {
+    fetch('https://api.buildnship.in/makemypass/integration/2f91a143-e4fd-446a-9edb-cd8909e9d59c/web-view/', { method: 'POST' });
+  }, []);
 
   return (
     <RouterProvider router={router} />
